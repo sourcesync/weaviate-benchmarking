bd61b3869df862035dfca29480c2e9262f935f84
Jacob Benson <jbenson116@gmail.com>
2023-06-23 10:19:41 -0700

    benchmark file modifications

--- benchmark-scripts/ann/benchmark.py
+++ benchmark-scripts/ann/benchmark.py
@@ -5,13 +5,16 @@ if __name__ == '__main__':
 
     # variables
     weaviate_url = 'http://weaviate:8084'
-    CPUs = 32
+    CPUs = 104
     efConstruction_array = [64, 128]
     maxConnections_array = [16, 32]
     ef_array = [64, 128, 256, 512]
+    start = 1000000
+    increment = 1000000
+    stop = 9000000
 
     benchmark_file_array = [
-        ['deep-20M.hdf5', 'cosine'],
+        ['deep-image-96-angular.hdf5', 'cosine'],
         # ['mnist-784-euclidean.hdf5', 'l2-squared'],
         # ['gist-960-euclidean.hdf5', 'l2-squared'],
         # ['glove-25-angular.hdf5', 'cosine']
@@ -19,4 +22,4 @@ if __name__ == '__main__':
  
     # Starts the actual benchmark, prints "completed" when done
     print("bm file", benchmark_file_array)
-    run_the_benchmarks(weaviate_url, CPUs, efConstruction_array, maxConnections_array, ef_array, benchmark_file_array)
+    run_the_benchmarks(weaviate_url, CPUs, efConstruction_array, maxConnections_array, ef_array, benchmark_file_array, start, increment, stop)
